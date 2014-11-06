3046c0c48b32b5b705e6cd6bdad95c57b5c9d93c
Benjamin Schmid <bene.schmid@gmail.com>
2014-11-06 14:11:33 +0100

    try to compile without --use-local-env

--- compile_jni.bat
+++ compile_jni.bat
@@ -1,5 +1,7 @@
 :: nvcc -O2 --cl-version 2010 --use-local-env fmvd_transform_cuda.cu
 
-nvcc --shared -O2 --cl-version 2010 --use-local-env -lcufft --include-path "c:/Program Files/Java/jdk1.7.0_60/include" --include-path "c:/Program Files/Java/jdk1.7.0_60/include/win32" -o NativeSPIMReconstructionCuda.dll fmvd_transform_cuda.cu fastspim_NativeSPIMReconstructionCuda.cpp convolutionFFT2D.cu fmvd_deconvolve_cuda.cpp fmvd_deconvolve_common.cpp fmvd_deconvolve_cuda_hdd.cpp
+:: nvcc --shared -O2 --cl-version 2012 --use-local-env -lcufft --include-path "c:/Program Files/Java/jdk1.7.0_51/include" --include-path "c:/Program Files/Java/jdk1.7.0_51/include/win32" -o NativeSPIMReconstructionCuda.dll fmvd_transform_cuda.cu fastspim_NativeSPIMReconstructionCuda.cpp convolutionFFT2D.cu fmvd_deconvolve_cuda.cpp fmvd_deconvolve_common.cpp fmvd_deconvolve_cuda_hdd.cpp
+
+nvcc --shared -O2 -lcufft --include-path "c:/Program Files/Java/jdk1.7.0_51/include" --include-path "c:/Program Files/Java/jdk1.7.0_51/include/win32" -o NativeSPIMReconstructionCuda.dll fmvd_transform_cuda.cu fastspim_NativeSPIMReconstructionCuda.cpp convolutionFFT2D.cu fmvd_deconvolve_cuda.cpp fmvd_deconvolve_common.cpp fmvd_deconvolve_cuda_hdd.cpp
 
 copy /Y NativeSPIMReconstructionCuda.dll ..\..\Fiji.app
