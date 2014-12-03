4ffdb167296ad9fe62b000805dd88be0343e9940
Benjamin Schmid <bene.schmid@gmail.com>
2014-12-03 10:05:00 +0100

    Get rid of the compile scripts.

--- compile_cuda.bat
+++
@@ -1,1 +0,0 @@
-nvcc -O2 --cl-version 2010 --use-local-env -lcufft fmvd_transform_cuda.cu convolutionFFT2D.cu fmvd_deconvolve_cuda.cpp fmvd_deconvolve_common.cpp fmvd_deconvolve_cuda_hdd.cpp
--- compile_fftw.bat
+++
@@ -1,1 +0,0 @@
-nvcc -O2 --cl-version 2010 --use-local-env -Xcompiler /EHsc,/Zi,/MT -L fftw-3.3.4-dll64 -llibfftw3f-3 fmvd_deconvolve_fftw.cpp fmvd_deconvolve_common.cpp
