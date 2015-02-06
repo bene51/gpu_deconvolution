b55a4de0ff9262bfcfb6ed2232edc78e61740f78
Benjamin Schmid <bene.schmid@gmail.com>
2015-02-06 16:41:47 +0100

    Fix a bug.
    After the loop, too many planes were returned.

--- fmvd_deconvolve.cpp
+++ fmvd_deconvolve.cpp
@@ -468,12 +468,11 @@ fmvd_deconvolve_planes_cuda(const struct fmvd_plan_cuda *plan, int iterations)
 	}
 
 	// save the remaining nStreams planes
-	stream_idx = stream_idx % plan->nStreams;
-	for(z = 0; z < plan->nStreams; z++) {
+	for(z = 0; z < plan->nStreams - 1; z++) {
+		stream_idx = (stream_idx + 1) % plan->nStreams;
 		int dataOffset = stream_idx * plan->dataW * plan->dataH;
 		checkCudaErrors(cudaStreamSynchronize(plan->streams[stream_idx]));
 		plan->return_next_plane(plan->h_Data[0] + dataOffset);
-		stream_idx = (stream_idx + 1) % plan->nStreams;
 	}
 
 #ifdef _WIN32
