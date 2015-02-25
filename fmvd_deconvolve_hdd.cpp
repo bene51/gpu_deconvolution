5b60eda65941726f86f017eac384a141845a2a31
Benjamin Schmid <bene.schmid@gmail.com>
2015-02-25 14:22:39 +0100

    Also create a standalone executable.

--- fmvd_deconvolve_hdd.cpp
+++ fmvd_deconvolve_hdd.cpp
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct iodata {
 	FILE **dataFiles;
@@ -88,52 +89,155 @@ load(int w, int h, float *data, const char *path)
 	fclose(f);
 }
 
-/*
+static void print_usage()
+{
+	printf("Usage:\n");
+	printf("fmvd_deconvolve -v <nViews>\n");
+	printf("                -i <nIterations>\n");
+	printf("                -t <iteration type>\n");
+	printf("                -o <output file>\n");
+	printf("                -d <datafile view1> <datafile view2> ...\n");
+	printf("                -w <weightfile view1> <weightfile view2> ...\n");
+	printf("                -k <kernelfile view1> <kernelfile view2> ...\n");
+	printf("                -dw <data width>\n");
+	printf("                -dh <data height>\n");
+	printf("                -dd <data depth>\n");
+	printf("                -kw <kernel width>\n");
+	printf("                -kh <kernel height>\n");
+}
+
 int
 main(int argc, char **argv)
 {
-	int it;
-	printf("[%s] - Starting...\n", argv[0]);
 
-	const int W = 600;
-	const int H = 600;
-	const int D = 600;
-	const int SEEK = 0;
+	int i = 1;
+	int nViews, nIterations, w, h, d, kw, kh, iterationType;
 
-	const int KW = 83;
-	const int KH = 83;
+	printf("argc = %d\n", argc);
 
-	float **kernel = (float **)malloc(2 * sizeof(float *));
-	kernel[0] = (float *)fmvd_malloc(KW * KH * sizeof(float));
-	kernel[1] = (float *)fmvd_malloc(KW * KH * sizeof(float));
-
-	load(KW, KH, kernel[0], "E:\\SPIM5_Deconvolution\\m6\\cropped\\forplanewisedeconv\\psf3.raw");
-	load(KW, KH, kernel[1], "E:\\SPIM5_Deconvolution\\m6\\cropped\\forplanewisedeconv\\psf5.raw");
-
-	FILE **dataFiles = (FILE**)malloc(2 * sizeof(FILE *));
-	char path[256];
-	for(it = 3; it < 4; it++) {
-		dataFiles[0] = fopen("E:\\SPIM5_Deconvolution\\m6\\cropped\\forplanewisedeconv\\v3.raw", "rb");
-		dataFiles[1] = fopen("E:\\SPIM5_Deconvolution\\m6\\cropped\\forplanewisedeconv\\v5.raw", "rb");
-		fseek(dataFiles[0], SEEK * W * H * sizeof(float), SEEK_SET);
-		fseek(dataFiles[1], SEEK * W * H * sizeof(float), SEEK_SET);
-
-		sprintf(path, "E:\\SPIM5_Deconvolution\\m6\\cropped\\forplanewisedeconv\\v35.deconvolved.gpu.raw");
-		FILE *resultFile = fopen(path, "wb");
-
-		fmvd_deconvolve_files_cuda(dataFiles, resultFile, W, H, D, kernel, KH, KW, 2, it);
-
-		fclose(dataFiles[0]);
-		fclose(dataFiles[1]);
-		fclose(resultFile);
+	if (argc == 1 || strcmp(argv[i++], "-v")) {
+		print_usage();
+		return -1;
 	}
 
-	fmvd_free(kernel[0]);
-	fmvd_free(kernel[1]);
+	nViews = atoi(argv[i++]);
+
+	char **datafiles = (char **)malloc(nViews * sizeof(char *));
+	char **weightfiles = (char **)malloc(nViews * sizeof(char *));
+	char **kernelfiles = (char **)malloc(nViews * sizeof(char *));
+	char *outputfile = NULL;
+	int tmp;
+
+	for(; i < argc; i++) {
+		if(!strcmp(argv[i], "-i"))
+			nIterations = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-t"))
+			iterationType = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-dw"))
+			w = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-dh"))
+			h = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-dd"))
+			d = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-kw"))
+			kw = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-kh"))
+			kh = atoi(argv[++i]);
+		else if(!strcmp(argv[i], "-o"))
+			outputfile = argv[++i];
+		else if(!strcmp(argv[i], "-d")) {
+			for(tmp = 0; tmp < nViews; tmp++)
+				datafiles[tmp] = argv[++i];
+		}
+		else if(!strcmp(argv[i], "-w")) {
+			for(tmp = 0; tmp < nViews; tmp++)
+				weightfiles[tmp] = argv[++i];
+		}
+		else if(!strcmp(argv[i], "-k")) {
+			for(tmp = 0; tmp < nViews; tmp++)
+				kernelfiles[tmp] = argv[++i];
+		}
+	}
+
+	printf("Starting plane-wise deconvolution\n");
+	printf("	nViews:        %d\n", nViews);
+	printf("	nIterations:   %d\n", nIterations);
+	printf("	iterationType: %d\n", iterationType);
+	printf("	dimensions:    %d x %d x %d\n", w, h, d);
+	printf("        kernel dims:   %d x %d\n", kw, kh);
+	printf("        data files:\n");
+	for(tmp = 0; tmp < nViews; tmp++)
+		printf("              %s\n", datafiles[tmp]);
+	printf("        weight files:\n");
+	for(tmp = 0; tmp < nViews; tmp++)
+		printf("              %s\n", weightfiles[tmp]);
+	printf("        kernel files:\n");
+	for(tmp = 0; tmp < nViews; tmp++)
+		printf("              %s\n", kernelfiles[tmp]);
+
+
+	// Read kernels
+	float **kernel = (float **)malloc(nViews * sizeof(float *));
+	for(int v = 0; v < nViews; v++) {
+		kernel[v] = (float *)fmvd_malloc(kw * kh * sizeof(float));
+		FILE *f = fopen(kernelfiles[v], "rb");
+		if(!f) {
+			printf("Could not open %s for reading\n", kernelfiles[v]);
+			exit(-1);
+		}
+		fread(kernel[v], sizeof(float), kw * kh, f);
+		fclose(f);
+	}
+
+	// Read weights
+	data_t **weights = (data_t **)malloc(nViews * sizeof(data_t *));
+	int datasize = w * h;
+	for(int v = 0; v < nViews; v++) {
+		weights[v] = (data_t *)malloc(datasize * sizeof(data_t));
+		FILE *f = fopen(weightfiles[v], "rb");
+		if(!f) {
+			printf("Could not open %s for reading\n", weightfiles[v]);
+			exit(-1);
+		}
+		fread(weights[v], sizeof(data_t), datasize, f);
+		fclose(f);
+	}
+
+	// Open input files
+	FILE **dataFiles = (FILE**)malloc(nViews * sizeof(FILE *));
+	for(int v = 0; v < nViews; v++) {
+		dataFiles[v] = fopen(datafiles[v], "rb");
+		if(!dataFiles[v]) {
+			printf("Could not open %s for reading\n", datafiles[v]);
+			exit(-1);
+		}
+	}
+
+	// Open output file
+	FILE *resultFile = fopen(outputfile, "wb");
+	if(!resultFile) {
+		printf("Could not open %s for reading\n", outputfile);
+		exit(-1);
+	}
+
+	// Do the deconvolution
+	fmvd_psf_type iteration_type = (fmvd_psf_type)iterationType;
+	fmvd_deconvolve_files_cuda(dataFiles, resultFile, w, h, d, weights, kernel, kh, kw, iteration_type, nViews, nIterations);
+
+	// Close input and output files
+	for(int v = 0; v < nViews; v++)
+		fclose(dataFiles[v]);
+	fclose(resultFile);
+
+	// Cleanup
+	for(int v = 0; v < nViews; v++) {
+		fmvd_free(kernel[v]);
+		free(weights[v]);
+	}
 	free(kernel);
+	free(weights);
 	free(dataFiles);
 
 	exit(EXIT_SUCCESS);
 }
 
-*/
